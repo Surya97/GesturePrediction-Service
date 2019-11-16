a2da48b1df8acf7166c1ca51bccdb4999ce7937c
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-16 03:25:24 -0700

    Updated model_training script

--- model_tuning.py
+++ model_tuning.py
@@ -24,8 +24,34 @@ X = features_obj.get_features()
 Y = [obj.label for obj in pose_objects]
 
 print(len(X), len(Y))
-clf_rforest = Classification('RForest', X, Y)
-clf_rforest.get_classifier_object()
-clf_rforest.get_metrics()
-pickle.dump(clf_rforest.get_classifier(), open('RForest_model.pkl', 'wb'))
+# clf_rforest = Classification('RForest', X, Y)
+# clf_rforest.get_classifier_object()
+# clf_rforest.get_metrics()
+# pickle.dump(clf_rforest.get_classifier(), open('RForest_model.pkl', 'wb'))
+# print()
+
+# clf_svm = Classification('svm', X, Y)
+# clf_svm.get_classifier_object()
+# clf_svm.get_metrics()
+# pickle.dump(clf_svm.get_classifier(), open('SVM_model.pkl', 'wb'))
+# print()
+
+# clf_knn = Classification('knn', X, Y)
+# clf_knn.get_classifier_object()
+# clf_knn.get_metrics()
+# pickle.dump(clf_knn.get_classifier(), open('KNN_model.pkl', 'wb'))
+# print()
+#
+# clf_mlp = Classification('mlp', X, Y)
+# clf_mlp.get_classifier_object()
+# clf_mlp.get_metrics()
+# pickle.dump(clf_mlp.get_classifier(), open('MLP_model.pkl', 'wb'))
+# print()
+
+clf_dt = Classification('logreg', X, Y)
+clf_dt.get_classifier_object()
+clf_dt.get_metrics()
+pickle.dump(clf_dt.get_classifier(), open('LogReg_model.pkl', 'wb'))
 print()
+
+
