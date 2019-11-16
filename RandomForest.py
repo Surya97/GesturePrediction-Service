63641981808d1a1b426469c73127523bfce6e878
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-15 19:06:51 -0700

    Removed K-fold cross validation

--- RandomForest.py
+++ RandomForest.py
@@ -5,11 +5,11 @@ from sklearn.model_selection import GridSearchCV
 class RandomForest:
     def __init__(self, x_train, y_train, x_test, y_test):
         self.clf = RandomForestClassifier(n_estimators=100)
-        self.grid_search_params = {
-            'min_samples_split': [2, 5, 10],
-            'n_estimators': [200, 400, 600, 800, 1000]
-        }
-        self.clf = GridSearchCV(self.clf, self.grid_search_params, cv=10)
+        # self.grid_search_params = {
+        #     'min_samples_split': [2, 5, 10],
+        #     'n_estimators': [200, 400, 600, 800, 1000]
+        # }
+        # self.clf = GridSearchCV(self.clf, self.grid_search_params, cv=10)
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
