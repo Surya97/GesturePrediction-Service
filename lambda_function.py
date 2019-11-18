acf5ac9f228eafa2efc9722f05e4c6756455f4ac
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-17 19:49:49 -0700

    Modified Lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -26,24 +26,24 @@ def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
 
-    random_forest_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("RForest_model.pkl").get()['Body'].read())
-
     svm_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("SVM_model.pkl").get()['Body'].read())
-
-    knn_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("KNN_model.pkl").get()['Body'].read())
 
     logreg_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("LogReg_model.pkl").get()['Body'].read())
 
+    lda_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("LDA_model.pkl").get()['Body'].read())
+
+    random_forest_classifier = pickle.loads(s3.Bucket("gesture-recognition").Object("RForest_model.pkl").get()['Body'].read())
+
     prediction_rf = random_forest_classifier.predict(features)
     prediction_svm = svm_classifier.predict(features)
-    prediction_knn = knn_classifier.predict(features)
+    prediction_lda = lda_classifier.predict(features)
     prediction_logreg = logreg_classifier.predict(features)
 
     data = {
-        "1": prediction_rf[0],
-        "2": prediction_svm[0],
-        "3": prediction_knn[0],
-        "4": prediction_logreg[0]
+        "1": prediction_svm[0],
+        "2": prediction_logreg[0],
+        "3": prediction_lda[0],
+        "4": prediction_rf[0]
     }
     return {
         'statusCode': 200,
